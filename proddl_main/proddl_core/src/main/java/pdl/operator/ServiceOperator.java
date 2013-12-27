62de42ea68a3826e671665320948b63524927640
Hyunsoo Daniel Kim <hkim500@gmil.com>
2013-12-27 15:22:07 -0500

    added flagged parameter parser

--- proddl_main/proddl_core/src/main/java/pdl/operator/ServiceOperator.java
+++ proddl_main/proddl_core/src/main/java/pdl/operator/ServiceOperator.java
@@ -21,6 +21,10 @@
 
 package pdl.operator;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
 /**
  * Created by IntelliJ IDEA.
  * User: hkim
@@ -35,6 +39,35 @@ public class ServiceOperator {
      * @param args
      */
     public static void main(String[] args) {
+        List<String> argsList = new ArrayList<String>();
+        HashMap<String, String> optsList = new HashMap<String, String>();
+        List<String> doubleOptsList = new ArrayList<String>();
+
+        for (int i = 0; i < args.length; i++) {
+            switch (args[i].charAt(0)) {
+                case '-':
+                    if (args[i].length() < 2)
+                        throw new IllegalArgumentException("Not a valid argument: "+args[i]);
+                    if (args[i].charAt(1) == '-') {
+                        if (args[i].length() < 3)
+                            throw new IllegalArgumentException("Not a valid argument: "+args[i]);
+                        // --opt
+                        doubleOptsList.add(args[i].substring(2, args[i].length()));
+                    } else {
+                        if (args.length-1 == i)
+                            throw new IllegalArgumentException("Expected arg after: "+args[i]);
+                        // -opt
+                        optsList.put(args[i], args[i+1]);
+                        i++;
+                    }
+                    break;
+                default:
+                    // arg
+                    argsList.add(args[i]);
+                    break;
+            }
+        }
+
         ServiceOperatorHelper helper = new ServiceOperatorHelper();
         helper.run();
     }
